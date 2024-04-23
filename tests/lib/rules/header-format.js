a5ebb97261afcf022094286241ba7e2dd671c1c1
Rob Misasi <rob.misasi@gmail.com>
2024-04-23 18:13:26 -0400

    Implement support for checking, formatting, and inserting leading comments in Vue files. Update project documentation. Update dependencies.

--- tests/lib/rules/header-format.js
+++ tests/lib/rules/header-format.js
@@ -339,3 +339,99 @@ ruleTester.run("header-presence", rule, {
     },
   ],
 });
+
+const ruleTesterVue = new RuleTester({
+  languageOptions: { parser: require("vue-eslint-parser"), ecmaVersion: 2018 },
+});
+
+const enableVueOption = { enableVueSupport: true };
+ruleTesterVue.run("header-presence-html", rule, {
+  valid: [
+    {
+      name: "Correctly matches a valid HTML header comment with the correct content",
+      filename: "test.vue",
+      options: [
+        { source: "string", content: "This is a header", ...enableVueOption },
+      ],
+      code: "<!--\n  This is a header\n-->\n<template>\n  <div>This is content</div>\n</template>\n",
+    },
+    {
+      name: "Correctly matches a valid HTML header comment with the correct content when the leading tag is not <template />",
+      filename: "test.vue",
+      options: [
+        { source: "string", content: "This is a header", ...enableVueOption },
+      ],
+      code: "<!--\n  This is a header\n-->\n<style>div { font-size: 2em; }</style>\n<template>\n  <div>This is content</div>\n</template>\n",
+    },
+  ],
+  invalid: [
+    {
+      name: "Inserts a missing header comment",
+      filename: "test.vue",
+      options: [
+        { source: "string", content: "This is a header", ...enableVueOption },
+      ],
+      code: "<template>\n  <div>This is content</div>\n</template>\n",
+      errors: [{ messageId: "missingHeader" }],
+      output:
+        "<!--\n  This is a header\n-->\n<template>\n  <div>This is content</div>\n</template>\n",
+    },
+    {
+      name: "Fixes a header comment with mismatched content",
+      filename: "test.vue",
+      options: [
+        {
+          source: "string",
+          content: "This is the expected header",
+          ...enableVueOption,
+        },
+      ],
+      code: "<!--\n  This is an incorrect header\n-->\n<template>\n  <div>This is content</div>\n</template>\n",
+      errors: [{ messageId: "headerContentMismatch" }],
+      output:
+        "<!--\n  This is the expected header\n-->\n<template>\n  <div>This is content</div>\n</template>\n",
+    },
+    {
+      name: "Inserts the missing header before a script tag",
+      filename: "test.vue",
+      options: [
+        {
+          source: "string",
+          content: "This is the expected header",
+          ...enableVueOption,
+        },
+      ],
+      code: "<!--\n  This is an incorrect header\n-->\n<script>import fakeModule from 'fakeModule';\nconst magicNum = 42;\n</script>\n<template>\n  <div>This is content</div>\n</template>\n",
+      errors: [{ messageId: "headerContentMismatch" }],
+      output:
+        "<!--\n  This is the expected header\n-->\n<script>import fakeModule from 'fakeModule';\nconst magicNum = 42;\n</script>\n<template>\n  <div>This is content</div>\n</template>\n",
+    },
+    {
+      name: "Inserts missing trailing newlines",
+      filename: "test.vue",
+      options: [
+        {
+          source: "string",
+          content: "This is a header",
+          trailingNewlines: 2,
+          ...enableVueOption,
+        },
+      ],
+      code: "<!--\n  This is a header\n-->\n<template>\n  <div>This is content</div>\n</template>\n",
+      errors: [{ messageId: "trailingNewlinesMismatch" }],
+      output:
+        "<!--\n  This is a header\n-->\n\n<template>\n  <div>This is content</div>\n</template>\n",
+    },
+    {
+      name: "Correctly identifies a missing header when another comment exists",
+      filename: "test.vue",
+      options: [
+        { source: "string", content: "This is a header", ...enableVueOption },
+      ],
+      code: "<template>\n  <div>This is content</div>\n</template>\n<!-- This is a footer -->",
+      errors: [{ messageId: "missingHeader" }],
+      output:
+        "<!--\n  This is a header\n-->\n<template>\n  <div>This is content</div>\n</template>\n<!-- This is a footer -->",
+    },
+  ],
+});
